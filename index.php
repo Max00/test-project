6ffe350946a0261f0261fd1ad87494bc20b8cd9d
Maxime Kieffer <maxime@kieffer.email>
2016-11-25 09:52:49 +0100

    HTML

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-// ...
-echo 'Bonjour';
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	
+</body>
+</html>
